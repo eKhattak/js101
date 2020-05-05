4cae15c6eea4e9cb13d92565725afcf27e28f4d7
Khattak <akkhattak65@gmail.com>
2020-05-05 14:34:47 +0500

    Added About

---
+++ about.js
@@ -0,0 +1,11 @@
+// 👨  Arsalan Khattak
+
+// 🏫  Numl Islamabad
+
+// 🏢  CARE Pvt Ltd
+
+// 🔗  https://twitter.com/khattakdev
+
+// 🔗  https://www.linkedin.com/in/arsalan-dilawar-56375a167/
+
+// 🔗  https://github.com/eKhattak
