a238be5933fa8ee1787d3e031ddf25461e741948
Khattak <akkhattak65@gmail.com>
2020-05-05 21:13:57 +0500

    Final

--- variables.js
+++ variables.js
@@ -1,11 +1,17 @@
 // Primitive Values
 
 // Strings
-
+var name = "Arsalan";
 // Numbers
-
+var num = 5;
 // Booleans
-
+var isAuth = true;
+var isLoggedIn = false;
 // null
-
+var iAmNull = null;
 // Undefined
+var iAmUndefined = undefined;
+
+var IAmAlsoUndefined;
+
+console.log(name, num, isAuth, isLoggedIn);
