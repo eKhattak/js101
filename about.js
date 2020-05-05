a632bd6c3d3c42b4b98c43b1afe7c620d1ed9281
Khattak <akkhattak65@gmail.com>
2020-05-05 14:40:33 +0500

    Updated About

--- about.js
+++ about.js
@@ -1,9 +1,13 @@
+// I am
 // 👨  Arsalan Khattak
 
+// Study at
 // 🏫  Numl Islamabad
 
+// Work at
 // 🏢  CARE Pvt Ltd
 
+// Available at
 // 🔗  https://twitter.com/khattakdev
 
 // 🔗  https://www.linkedin.com/in/arsalan-dilawar-56375a167/
